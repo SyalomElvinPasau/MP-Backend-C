fdf27c62c231b1d1c0607f79fb8ed9f59a494b46
Elvin <syalomelvinpasau@gmail.com>
2025-12-18 13:52:03 +0700

    fixing secure attribute

--- backend/utils/cookies.js
+++ backend/utils/cookies.js
@@ -1,21 +1,19 @@
 import { readJSON } from "./json.js";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-import { getSession } from "../controllers/sessionController.js";
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const USERS_PATH = join(__dirname, "../../data/users.json");
-const SESSION = join(__dirname, "../../data/session.json");
 
 const COOKIE_CONFIG = {
     httpOnly: true,
-    secure,
+    secure: 'Secure',
     sameSite: 'Strict',
     maxAge: 24 * 60 * 60,
     path: '/'
-};
+};  
 
 export function createCookie(name, value, options = {}) {
     const config = { ...COOKIE_CONFIG, ...options };
@@ -68,13 +66,9 @@ export async function getUserFromCookies(request) {
     const cookies = parseCookies(request);
     // console.log("Cookie: ", cookies);
     const sessionId = cookies.sessionId;
-    if(!sessionId) return null;
-
-    const session = await getSession(sessionId);
-    if (!session) return null;
 
     const users = await readJSON(USERS_PATH);
-    const user = users.find(u => u.id === session.userId);
+    const user = users.find(u => u.sessionId == sessionId);
 
     return user || null; //null if not logged in
 }
