3959f10985e5054945f8e068bb736df0f016b5c4
FeliksMindoTuaSitorus <felikssitorus@gmail.com>
2025-12-18 13:02:59 +0700

    feat: add session

---
+++ backend/controllers/sessionController.js
@@ -0,0 +1,45 @@
+import { readJSON, writeJSON } from "../utils/json.js";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const SESSION = join(__dirname, "../../data/session.json");
+const DURASI_SESSION = 30 * 60 * 1000;
+
+export async function createSession(sessionId, userId) {
+    const session = await readJSON(SESSION);
+
+    session[sessionId] = {
+        userId: userId,
+        createdAt: Date.now(),
+    }
+
+    await writeJSON(SESSION, session);
+}
+
+export async function getSession(sessionId) {
+    const sessions = await readJSON(SESSION);
+    const session = sessions[sessionId];
+
+    if (!session) return null;
+
+    if (Date.now() - session.createdAt > DURASI_SESSION) {
+        delete sessions[sessionId];
+        await writeJSON(SESSION, sessions);
+        return null;
+    }
+
+    session.createdAt = Date.now();
+    sessions[sessionId] = session;
+    await writeJSON(SESSION, sessions);
+
+    return session;
+}
+
+export async function deleteSession(sessionId) {
+    const session = await readJSON(SESSION);
+    delete session[sessionId];
+    await writeJSON(SESSION, session);
+}
+
--- backend/utils/cookies.js
+++ backend/utils/cookies.js
@@ -1,11 +1,13 @@
 import { readJSON } from "./json.js";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import { getSession } from "../controllers/sessionController.js";
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const USERS_PATH = join(__dirname, "../../data/users.json");
+const SESSION = join(__dirname, "../../data/session.json");
 
 export function parseCookies(request) {
     const cookieHeader = request.headers?.cookie;
@@ -26,9 +28,13 @@ export async function getUserFromCookies(request) {
     const cookies = parseCookies(request);
     // console.log("Cookie: ", cookies);
     const sessionId = cookies.sessionId;
+    if(!sessionId) return null;
+
+    const session = await getSession(sessionId);
+    if (!session) return null;
 
     const users = await readJSON(USERS_PATH);
-    const user = users.find(u => u.sessionId == sessionId);
+    const user = users.find(u => u.id === session.userId);
 
     return user || null; //null if not logged in
 }
